01d174aebd84691ecd76bf6ab032131c486d9419
masab5 <masabhd5@gmail.com>
2022-01-20 11:10:36 +0600

    Works fine with tmux compiler

--- Scripts/pycompiler.sh
+++
@@ -1,9 +0,0 @@
-#!~/../usr/bin/bash
-
-#tmux split-window
-#tmux resize-p -D 6
-tmux select-pane -t 1
-tmux send 'python3 "$(<~/.filename.txt)"' ENTER;
-tmux select-pane -t 0
-tmux send ENTER;
-tmux select-pane -t 1
